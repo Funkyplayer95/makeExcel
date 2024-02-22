5028ac410b66189b8b13cce5b7db8ea4abfb32fe
Funkyplayer95 <funkyplayer95@kakao.com>
2024-02-22 16:50:54 +0900

    주석 추가
    파일첨부 제외 나머지 기능 완성.
    추가 설명부분 주석으로 진행.

--- ExcelData/src/main/java/makeExcel/ExcelData/Controller/DataController.java
+++ ExcelData/src/main/java/makeExcel/ExcelData/Controller/DataController.java
@@ -15,81 +15,115 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
 public class DataController {
-    @Autowired
+    @Autowired // 의존성 주입을 할때 사용하는 어노테이션
     private final DataService dataService;
 
-    @GetMapping("/")
+    @GetMapping("/") // 기본 페이지 이동
     public String allDataList(Model model){
         return "makeExcel";
-    }
-    @GetMapping("/check")
+    } //html로 입장
+
+    @GetMapping("/check") // 조회버튼 클릭시
     public String dataCheck(Model model){
-        List<DataDTO> allData = dataService.getAllItemData();
-        model.addAttribute("allData", allData);
-        return "makeExcel";
-    }
-    @PostMapping("/save")
-    public String addData(@RequestBody DataDTO newData) {
-        dataService.saveData(newData);
+        List<DataDTO> allData = dataService.getAllItemData(); // 모든 데이터를 가져온다.
+        model.addAttribute("allData", allData); // "allData"라는 명칭에 주입
         return "makeExcel";
     }
 
-    @PostMapping("/deleteData")
+    @PostMapping("/save") // 저장버튼 클릭 시,
+    public String addData(@RequestBody DataDTO newData) { //@RequestBody는 html에서 보내는 데이터를 받는다
+        dataService.saveData(newData); //받은 데이터들을 save 진행.
+        return "makeExcel";
+    }
+
+    @PostMapping("/delete") // 삭제 버튼 클릭 시, 삭제하는 value를 가진 리스트를 받아온다. []로 받은 delete할 값들 받기
     public String deleteData(@RequestBody List<String> codesToDelete) {
-        for (String code : codesToDelete) {
-            dataService.deleteData(code);
+        for (String code : codesToDelete) { //리스트 안에 있는 value값을 조회
+            dataService.deleteData(code); // 삭제하도록 진행.
         }
         return "redirect:/";
     }
 
+    @PostMapping("/download") // 엑셀 다운로드 버튼 누를 시
+    public void excelDownload(HttpServletResponse response) throws IOException { // throws IOException 예외처리
+        long itemCount = dataService.countItems(); // 데이터의 총 개수를 반환해서 저장
+        List<DataDTO> allData = dataService.getAllItemData(); // allData 안에 모든 데이터들을 저장
 
-//    @GetMapping("/excel/upload")
+        Workbook wb = new XSSFWorkbook(); // XSSFWorkbook 객체를 생성해서 wb에 저장. 엑셀파일 생성하는데 사용.
+        Sheet sheet = wb.createSheet("첫번째 시트"); // 엑셀 시트를 생성. 이름을 ()로 저장.
+        Row row = null; // 엑셀의 행
+        Cell cell = null; // 엑셀의 열
+        int rowNum = 0; // 행 번호
 
+        row = sheet.createRow(rowNum++); // 첫 번째 행 생성.
+        cell = row.createCell(0); // 첫 번째 열 생성.
+        cell.setCellValue("번호"); // 값을 번호로 지정.
+        cell = row.createCell(1); // 두 번째 열 생성.
+        cell.setCellValue("이름"); // 값을 이름 으로 지정
+        cell = row.createCell(2); // 세 번째 열 생성.
+        cell.setCellValue("가격"); // 값을 가격 으로 지정.
+        cell = row.createCell(3); // 네 번째 열 생성.
+        cell.setCellValue("판매수량"); // 값을 판매수량 으로 지정.
 
-
-    @PostMapping("/download")
-    public void excelDownload(HttpServletResponse response) throws IOException {
-        long itemCount = dataService.countItems();
-        List<DataDTO> allData = dataService.getAllItemData();
-
-        Workbook wb = new XSSFWorkbook();
-        Sheet sheet = wb.createSheet("첫번째 시트");
-        Row row = null;
-        Cell cell = null;
-        int rowNum = 0;
-
-        row = sheet.createRow(rowNum++);
-        cell = row.createCell(0);
-        cell.setCellValue("번호");
-        cell = row.createCell(1);
-        cell.setCellValue("이름");
-        cell = row.createCell(2);
-        cell.setCellValue("가격");
-        cell = row.createCell(3);
-        cell.setCellValue("판매수량");
-
-        for (int i=0; i < itemCount ; i++){
-            row = sheet.createRow(rowNum++);
-            cell = row.createCell(0);
-            cell.setCellValue(allData.get(i).getCode());
-            cell = row.createCell(1);
-            cell.setCellValue(allData.get(i).getName());
-            cell = row.createCell(2);
-            cell.setCellValue(allData.get(i).getPrice());
-            cell = row.createCell(3);
-            cell.setCellValue(allData.get(i).getSalesQ());
+        for (int i=0; i < itemCount ; i++){ // for문을 이용. 데이터값만큼 반복.
+            row = sheet.createRow(rowNum++); // 새로운 행 추가
+            cell = row.createCell(0); // 새로운 행에 첫 번째 열 추가
+            cell.setCellValue(allData.get(i).getCode()); // allData안의 i번째의 code를 얻어온다.
+            cell = row.createCell(1); // 새로운 행에 두 번째 열 추가
+            cell.setCellValue(allData.get(i).getName()); // allData안의 i번째의 name을 얻어온다.
+            cell = row.createCell(2); // 새로운 행에 세 번째 열 추가
+            cell.setCellValue(allData.get(i).getPrice()); // allData안의 i번째의 price를 얻어온다.
+            cell = row.createCell(3); // 새로운 행에 네 번째 열 추가
+            cell.setCellValue(allData.get(i).getSalesQ()); // allData안의 i번째의 salesQ를 얻어온다.
         }
-        response.setContentType("ms-vnd/excel");
-        response.setHeader("Content-Disposition", "attachment;filename=data.xlsx");
+        response.setContentType("ms-vnd/excel"); // 응답 컨텐츠 타입을 엑셀로 지정. MIME타입을 나타낸다 함
+        response.setHeader("Content-Disposition", "attachment;filename=data.xlsx"); // 파일이름을 저장 시 data.xlsx로 저장
 
-        wb.write(response.getOutputStream());
-        wb.close();
+        wb.write(response.getOutputStream()); // 생성된 엑셀 파일을 출력 스트림에 사용. 클라이언트가 다운받을 수 있게 함.
+        wb.close(); // 작업이 끝났으면 Workbook을 닫는다. 메모리 누수를 방지하기 위함.
+    }
+
+    @PostMapping("/upload")
+    public String handleFileUpload(@RequestParam("file") MultipartFile file) {
+        try {
+            List<DataDTO> dataDTOs = readExcelFile(file);
+            for (DataDTO dataDTO : dataDTOs) {
+                dataService.saveData(dataDTO);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
+
+    private List<DataDTO> readExcelFile(MultipartFile file) throws IOException {
+        List<DataDTO> dataDTOs = new ArrayList<>();
+
+        Workbook workbook = new XSSFWorkbook(file.getInputStream());
+        Sheet sheet = workbook.getSheetAt(0);
+
+        for (Row currentRow : sheet) {
+            int code = (int) currentRow.getCell(0).getNumericCellValue();
+            String name = currentRow.getCell(1).getStringCellValue();
+            int price = (int) currentRow.getCell(2).getNumericCellValue();
+            int salesQ = (int) currentRow.getCell(3).getNumericCellValue();
+
+            DataDTO dataDTO = new DataDTO(code, name, price, salesQ);
+            dataDTOs.add(dataDTO);
+        }
+
+        workbook.close();
+
+        return dataDTOs;
     }
 }
--- ExcelData/src/main/java/makeExcel/ExcelData/Service/DataService.java
+++ ExcelData/src/main/java/makeExcel/ExcelData/Service/DataService.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import makeExcel.ExcelData.DTO.DataDTO;
 import makeExcel.ExcelData.Entity.Data;
 import makeExcel.ExcelData.Repository.DataRepository;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,35 +22,38 @@ public class DataService {
     private final DataRepository dataRepository;
 
     public List<DataDTO> getAllItemData() {
-        List<Data> data = dataRepository.findAll();
-        List<DataDTO> dataAll = new ArrayList<>();
+        List<Data> data = dataRepository.findAll(); // findAll로 모든것을 찾아 안에 넣는다.
+        List<DataDTO> dataAll = new ArrayList<>(); // 새로운 리스트 객체를 생성
 
-        for (Data datalist : data){
-            DataDTO dataDTO =new DataDTO(
-                    datalist.getCode(),
-                    datalist.getName(),
-                    datalist.getPrice(),
-                    datalist.getSalesQ());
-            dataAll.add(dataDTO);
-        }   return dataAll;
+        for (Data dataList : data){ // dataList안에 data가 반복적으로 진행
+            DataDTO dataDTO =new DataDTO( // 새로운 DTO객체 생성
+                    dataList.getCode(), // code를 얻어온다
+                    dataList.getName(), // name을 얻어온다
+                    dataList.getPrice(), // price를 얻어온다
+                    dataList.getSalesQ()); // salesQ를 얻어온다
+            dataAll.add(dataDTO); // 얻어온 데이터를 dataAll안에 넣어둔다.
+        }   return dataAll; // getAllItemData는 dataAll을 반환한다.
     }
+
     public long countItems() {
         return dataRepository.count();
-    }
+    } // count를 사용해서 데이터 개수를 가져온다
+
 
     public DataDTO saveData(DataDTO dataDTO){
-        Data data = new Data();
+        Data data = new Data(); // 새로운 Data 객체 생성
 
-        data.setCode(dataDTO.getCode());
-        data.setName(dataDTO.getName());
-        data.setPrice(dataDTO.getPrice());
-        data.setSalesQ(dataDTO.getSalesQ());
+        data.setCode(dataDTO.getCode()); // DTO에 저장된 code를 entity에 set함
+        data.setName(dataDTO.getName()); // DTO에 저장된 naem을 entity에 set함
+        data.setPrice(dataDTO.getPrice()); // DTO에 저장된 price를 entity에 set함
+        data.setSalesQ(dataDTO.getSalesQ()); // DTO에 저장된 salesQ를 entity에 set함
 
-        data = dataRepository.save(data);
+        data = dataRepository.save(data); // update할 수 있도록 save문을 사용.
 
         return new DataDTO(data.getCode(), data.getName(), data.getPrice(),data.getSalesQ());
+        // 저장된 데이터를 이용, 새로운 DTO객체를 생성하고 반환. 클라이언트에게 저장된 데이터의 정보를 전달.
     }
     public void deleteData(String id) {
-        dataRepository.deleteById(id);
+        dataRepository.deleteById(id); // 인자로 받은 id값을 찾아서 데이터베이스에서 삭제.
     }
 }
